9f7f43c3c6deef441d74766de39409d1eb3f56ea
Rommel Dominguez <leonidastouch@icloud.com>
2019-04-20 21:31:00 +0800

    the hard way of revealing the values of an array

---
+++ arraytwo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int rommel[10] = {1,2,3,4,5,6,7,8,9,10};
+	printf("%9d\n",rommel[0]);
+	printf("%9d\n",rommel[1]);
+	printf("%9d\n",rommel[2]);
+	printf("%9d\n",rommel[3]);
+	printf("%9d\n",rommel[4]);
+	printf("%9d\n",rommel[5]);
+	printf("%9d\n",rommel[6]);
+	printf("%9d\n",rommel[7]);
+	printf("%9d\n",rommel[8]);
+	printf("%9d\n",rommel[9]);
+	
+	return 0;
+}
