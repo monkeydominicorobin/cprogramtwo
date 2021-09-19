55b072f68e7bf351fe9d841ae97accc6fec2b4e5
Rommel Dominguez <bm-2cu8dmto5ga5pf2xkvapdrqcpwtzybsxpw@riseup.net>
2021-09-19 15:34:29 +0800

    after more than two years no iteration

--- arraytwo.c
+++ arraytwo.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-	int rommel[10] = {1,2,3,4,5,6,7,8,9,10};
-	printf("%9d\n",rommel[0]);
-	printf("%9d\n",rommel[1]);
-	printf("%9d\n",rommel[2]);
-	printf("%9d\n",rommel[3]);
-	printf("%9d\n",rommel[4]);
-	printf("%9d\n",rommel[5]);
-	printf("%9d\n",rommel[6]);
-	printf("%9d\n",rommel[7]);
-	printf("%9d\n",rommel[8]);
-	printf("%9d\n",rommel[9]);
+	int monkey[10] = {1,2,3,4,5,6,7,8,9,10};
+	printf("%9d\n",monkey[0]);
+	printf("%9d\n",monkey[1]);
+	printf("%9d\n",monkey[2]);
+	printf("%9d\n",monkey[3]);
+	printf("%9d\n",monkey[4]);
+	printf("%9d\n",monkey[5]);
+	printf("%9d\n",monkey[6]);
+	printf("%9d\n",monkey[7]);
+	printf("%9d\n",monkey[8]);
+	printf("%9d\n",monkey[9]);
 	
 	return 0;
 }
